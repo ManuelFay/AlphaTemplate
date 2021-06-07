436f1b1e3cca2f980186f62ea4d733ea08c0d566
ManuelFay <manuel.faysse@epfl.ch>
2021-06-07 16:50:58 +0200

    fix order and training

--- alpha_template/interfaces/board.py
+++ alpha_template/interfaces/board.py
@@ -14,6 +14,7 @@ class Board:
         self.kernel = np.array([[0, 1, 0],
                                 [1, 0, 1],
                                 [0, 1, 0]])
+        self.action_indices = list(zip(*np.where(self.init_array() == 1)))
         assert isinstance(self.board, np.ndarray)
 
     @staticmethod
